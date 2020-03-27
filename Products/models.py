77c0985a5470dfd54858c182a19b76fb4b4b1fd8
Swapnil Raykar <swap612@gmail.com>
2020-03-27 12:14:14 +0530

    Created migrations

--- Products/models.py
+++ Products/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
+
+
